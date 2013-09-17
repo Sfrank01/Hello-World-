dddb72a97789bafa64bc42238bbd9a47d521cf10
sfrank <sfrank01@g.harvard.edu>
2013-09-17 20:15:31 +0200

    Practice

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset= "utf-8">
+		<title> Hello World</title>
+		</head>
+		
+		<body> <h1> Hello World</h1>
+			<p> Today&rsquo;s date (according to this web server) is
+			<?php
+			
+			echo date('l, F jS Y.');
+			
+			?>
+			</p>
+		</body>
+</html>		
+		
